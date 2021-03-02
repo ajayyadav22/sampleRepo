2216df31d4603e90e739e3a96e49fbb707ed93c2
Ajay Yadav <62737082+ajayyadav22@users.noreply.github.com>
2021-03-02 16:11:49 +0530

    Update sampleScript.sh

--- sampleScript.sh
+++ sampleScript.sh
@@ -1,1 +1,22 @@
+enabled=$1
+db_host=$2
+port=$3
+db_user=$4
+password=$5
+pipeline_name=$6
+source_tenant=$7
+dest_tenants=$8
 
+echo "running"
+
+if [ $enabled == "true" ]
+then
+echo "Starting RDS copy.... Will take around 2 hours."
+echo "host: $db_host ",
+echo "port: $port",
+echo "user: $db_user",
+echo "source_tenant: $source_tenant ",
+echo "dest_tenants: $dest_tenants",
+echo "aws profile: $AWS_PROFILE "
+echo "In test branch "
+fi
